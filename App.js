fe601ba386b3f43d1546aebeb0c33c9984b15481
Camilo Restrepo <camilorg2017@gmail.com>
2023-05-04 22:01:38 -0500

    create .apk for project android

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
 import HomeScreen from "./components/HomeScreen";
 import CharacterScreen from "./components/CharacterScreen";
 
@@ -9,25 +10,27 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerTitleAlign: "center",
-          headerTitleStyle: {
-            alignSelf: "center",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              alignSelf: "center",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
 
-        <Stack.Screen name="Character" component={CharacterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen name="Character" component={CharacterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
